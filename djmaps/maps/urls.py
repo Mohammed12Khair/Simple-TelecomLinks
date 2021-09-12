2e0601e5f13f521e5cb22daf146dac3a23e0676f
Mohammed Khair <55262573+Mohammed12Khair@users.noreply.github.com>
2021-09-12 21:03:44 +0200

    Restructure

--- djmaps/maps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url                                                                                                                              
-from . import views
-from django.urls import path
-
-urlpatterns = [ 
-    path('', views.default_map, name="default"),
-]
