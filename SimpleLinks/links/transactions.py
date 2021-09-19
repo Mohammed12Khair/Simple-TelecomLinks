fa9de2fe7d4c1906ccd00d4c4ea0d47182bd3adc
Mohammed Khair <55262573+Mohammed12Khair@users.noreply.github.com>
2021-09-19 08:20:10 +0200

    Edit

--- SimpleLinks/links/transactions.py
+++ SimpleLinks/links/transactions.py
@@ -17,5 +17,25 @@ class siteaction:
             try:
                 site_map.objects.filter(id=self.request.POST['id']).update(sitename=self.request.POST['sitename'],siteid=self.request.POST['siteid'],\
                     long=self.request.POST['long'],lat=self.request.POST['lat'],weight=self.request.POST['weight'])
+
             except Exception as e:
                 return str(e) + " #002"
+    def TableData(self):
+        html=''
+        counter=0
+        Table_Data=site_map.objects.all().order_by('-id')
+        for i in Table_Data:
+            html+='<tr>'
+            html+='<td>' + str(counter) + '</td>'
+            html+='<td class="' + str(i.id) + 'siteid">' + str(i.siteid) +  '</td>'
+            html+='<td class="' + str(i.id) + 'sitename">' + str(i.sitename) + '</td>'
+            html+='<td class="' +str( i.id) + 'long">' + str(i.long) + '</td>'
+            html+='<td class="' +str( i.id) + 'lat">' + str(i.lat) + '</td>'
+            html+='<td class="' +str( i.id) + 'weight">' + str(i.weight) + '</td>'
+            html+='<td class="' +str( i.id) + 'status">' + str(i.status) + '</td>'
+            html+='<td style="text-align: center;">'
+            html+='<button class="btn btn-sm btn-info edit_site_btn" data-toggle="modal" data-target="#editform" row="' + str(i.id) + '"><i class="fa fa-edit"></i> EDIT</button>'
+            html+='<button class="btn btn-sm btn-danger delete_btn" row="' + str(i.id) + '"><i class="fa fa-trash"></i> DELETE</button>'
+            html+='</td></tr>'
+        return html
+
--- SimpleLinks/links/views.py
+++ SimpleLinks/links/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponse, HttpResponseRedirect
+from django.http.response import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.core.exceptions import PermissionDenied
@@ -8,67 +8,92 @@ from .models import site_map
 from .transactions import siteaction
 
 # Create your views here.
+
+
 def dashboard(request):
-    return render(request,'home/dashboard.html',{})
+    return render(request, 'home/dashboard.html', {})
 
 # Site manger
-@permission_required('AddSite',raise_exception=True)
+
+
+@permission_required('AddSite', raise_exception=True)
 def add_site(request):
-    siteid_=siteAttr.objects.get(key='siteid')
-    sitename_=siteAttr.objects.get(key='sitename')
-    contex={
-        'siteid_':siteid_,
-        'sitename_':sitename_,
+    siteid_ = siteAttr.objects.get(key='siteid')
+    sitename_ = siteAttr.objects.get(key='sitename')
+    contex = {
+        'siteid_': siteid_,
+        'sitename_': sitename_,
     }
     if request.method == "POST":
-        error=''
-        siteid=request.POST['siteid']
-        sitename=request.POST['sitename']
-        longitude=request.POST['longitude']
-        Latitude=request.POST['Latitude']
-        site_weight=request.POST['site_weight']
+        error = ''
+        siteid = request.POST['siteid']
+        sitename = request.POST['sitename']
+        longitude = request.POST['longitude']
+        Latitude = request.POST['Latitude']
+        site_weight = request.POST['site_weight']
         try:
-            add_site_=site_map(site_id=siteid,site_name=sitename,site_long=longitude,site_lat=Latitude,site_weight=site_weight)
+            add_site_ = site_map(site_id=siteid, site_name=sitename,
+                                 site_long=longitude, site_lat=Latitude, site_weight=site_weight)
             add_site_.save()
-            error='Site ' + str(siteid) + ' added Succssfuly' 
+            error = 'Site ' + str(siteid) + ' added Succssfuly'
         except Exception as e:
-            error=str(r)
-        contex={
-            'siteid_':siteid_,
-            'sitename_':sitename_,
-            'error':error,
-            }
-            
-    return render(request,'sitemanger/add_site.html',contex)
+            error = str(r)
+        contex = {
+            'siteid_': siteid_,
+            'sitename_': sitename_,
+            'error': error,
+        }
+
+    return render(request, 'sitemanger/add_site.html', contex)
 
 
 def site_manger(request):
-    site_map_data=site_map.objects.all()
-    siteid_=siteAttr.objects.get(key='siteid')
-    sitename_=siteAttr.objects.get(key='sitename')
-    contex={
-        'siteid_':siteid_,
-        'sitename_':sitename_,
-        'site_map_data':site_map_data,
+    site_map_data = site_map.objects.all().order_by('-id')
+    siteid_ = siteAttr.objects.get(key='siteid')
+    sitename_ = siteAttr.objects.get(key='sitename')
+    contex = {
+        'siteid_': siteid_,
+        'sitename_': sitename_,
+        'site_map_data': site_map_data,
     }
     if request.method == "POST":
         if request.POST['action'] == "check_site_name" and request.is_ajax():
-            if site_map.objects.filter(siteid= request.POST['siteid']).exists():
+            if site_map.objects.filter(siteid=request.POST['siteid']).exists():
                 return HttpResponse('1')
             else:
                 return HttpResponse('0')
-
-        if request.POST['action'] == "site_manger_add":
-            DB=siteaction('site_manger_add',request)
+        if request.POST['action'] == "site_manger_add" and request.is_ajax():
+            DB = siteaction('site_manger_add', request)
+            if DB.commit() is not None:
+                status = {
+                    'error': '1',
+                    'msg': DB.commit(),
+                    'TableData':DB.TableData()
+                }
+            else:
+                status={
+                    'error': '0',
+                    'msg': DB.commit(),
+                    'TableData':DB.TableData()
+                }
+            return JsonResponse(status,safe=False)
         if request.POST['action'] == "site_manger_edit":
-            DB=siteaction('site_manger_edit',request)
-        if DB.commit() is not None:
-            contex['msg']=DB.commit()
-    return render(request,'sitemanger/index.html',contex)
+            DB = siteaction('site_manger_edit', request)
+            if DB.commit() is not None:
+                contex['msg'] = DB.commit()
+    return render(request, 'sitemanger/index.html', contex)
 
-def site_manger_delete(request,id):
+
+def site_manger_delete(request):
     try:
-        site_map.objects.filter(id=id).delete()
+        site_map.objects.filter(id=request.POST['id']).delete()
+        status = {
+            'error': '0',
+            'msg': 'Site removed from list'
+        }
     except Exception as e:
-        str(e)
-    return HttpResponseRedirect('../site_manger')
+        status = {
+            'error': '1',
+            'msg': 'Failed'
+        }
+    return JsonResponse(status, safe=False)
